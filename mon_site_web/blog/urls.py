419ed94c9767e1994cd5ce57596cb83406a4b352
GregoryToc <gregory.toccanier@gmail.com>
2025-06-02 14:31:44 +0200

    Add 4 pages

--- mon_site_web/blog/urls.py
+++ mon_site_web/blog/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('ajouter/', views.ajouter_article, name='ajouter_article'),
     path('article/<int:article_id>/', views.detail_article, name='detail_article'),
+    path('articles/', views.articles, name='articles'),
+    path('categories/', views.categories, name='categories'),
+    path('auteurs/', views.auteurs, name='auteurs'),
+    path('a-propos/', views.a_propos, name='a_propos'),
 ]
--- mon_site_web/blog/views.py
+++ mon_site_web/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
+from django.db.models import Count, Max
 from .models import Article, Commentaire, Categorie
 from .forms import ArticleForm, CommentaireForm
 
@@ -63,4 +64,67 @@ def detail_article(request, article_id):
         'article': article,
         'commentaires': commentaires,
         'form': form
+    })
+
+
+def articles(request):
+    """Vue pour afficher tous les articles avec pagination et tri"""
+    articles_list = Article.objects.all().order_by('-date_creation')
+    
+    # Filtre par catégorie si demandé
+    categorie_id = request.GET.get('categorie')
+    if categorie_id:
+        articles_list = articles_list.filter(categorie_id=categorie_id)
+    
+    # Tri si demandé
+    sort_by = request.GET.get('sort', '-date_creation')
+    if sort_by in ['date_creation', '-date_creation', 'titre', '-titre']:
+        articles_list = articles_list.order_by(sort_by)
+    
+    categories = Categorie.objects.all()
+    
+    return render(request, 'blog/articles.html', {
+        'articles': articles_list,
+        'categories': categories,
+        'categorie_selectionnee': categorie_id,
+        'sort_actuel': sort_by
+    })
+
+
+def categories(request):
+    """Vue pour afficher toutes les catégories avec leurs statistiques"""
+    categories_list = Categorie.objects.annotate(
+        nb_articles=Count('articles')
+    ).order_by('-nb_articles')
+    
+    return render(request, 'blog/categories.html', {
+        'categories': categories_list
+    })
+
+
+def auteurs(request):
+    """Vue pour afficher tous les auteurs avec leurs statistiques"""
+    # Récupérer tous les auteurs uniques avec le nombre d'articles
+    auteurs_list = Article.objects.values('auteur').annotate(
+        nb_articles=Count('id'),
+        dernier_article=Max('date_creation')
+    ).order_by('-nb_articles')
+    
+    return render(request, 'blog/auteurs.html', {
+        'auteurs': auteurs_list
+    })
+
+
+def a_propos(request):
+    """Vue pour la page À propos"""
+    # Statistiques du blog
+    stats = {
+        'total_articles': Article.objects.count(),
+        'total_commentaires': Commentaire.objects.count(),
+        'total_categories': Categorie.objects.count(),
+        'total_auteurs': Article.objects.values('auteur').distinct().count(),
+    }
+    
+    return render(request, 'blog/a_propos.html', {
+        'stats': stats
     })
